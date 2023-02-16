1b8985d4de002a0dc5967269d5ebbc5655337291
Willard Owiti Odongo <104354920+Willard254@users.noreply.github.com>
2023-02-16 18:59:44 +0300

    Add files via upload

---
+++ GROUP DIABETES PROJECT/GROUP DIABETES PROJECT/group.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+
+
+
+df = pd.read_csv('/content/diabetes.csv')
+
+# HEADINGS
+st.title('FEMALE DIABETES PREDICTOR')
+st.sidebar.header('Patient Data')
+st.subheader('Training Data Stats')
+st.write(df.describe())
+
+
+# X AND Y DATA
+x = df.drop(['Outcome'], axis = 1)
+y = df.iloc[:, -1]
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
+
+
+# FUNCTION
+def user_report():
+  pregnancies = st.sidebar.slider('Pregnancies', 0,17, 3 )
+  glucose = st.sidebar.slider('Glucose', 0,200, 120 )
+  bp = st.sidebar.slider('Blood Pressure', 0,122, 70 )
+  skinthickness = st.sidebar.slider('Skin Thickness', 0,100, 20 )
+  insulin = st.sidebar.slider('Insulin', 0,846, 79 )
+  bmi = st.sidebar.slider('BMI', 0,67, 20 )
+  dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0,2.4, 0.47 )
+  age = st.sidebar.slider('Age', 21,88, 33 )
+
+  user_report_data = {
+      'pregnancies':pregnancies,
+      'glucose':glucose,
+      'bp':bp,
+      'skinthickness':skinthickness,
+      'insulin':insulin,
+      'bmi':bmi,
+      'dpf':dpf,
+      'age':age
+  }
+  report_data = pd.DataFrame(user_report_data, index=[0])
+  return report_data
+
+
+
+
+# PATIENT DATA
+user_data = user_report()
+st.subheader('Patient Data')
+st.write(user_data)
+
+
+
+
+# MODEL
+rf  = RandomForestClassifier()
+rf.fit(x_train, y_train)
+user_result = rf.predict(user_data)
+
+
+
+# VISUALISATIONS
+st.title('Visualised Patient Report')
+
+
+
+# COLOR FUNCTION
+if user_result[0]==0:
+  color = 'blue'
+else:
+  color = 'red'
+
+
+# OUTPUT
+st.subheader('Your Report: ')
+output=''
+if user_result[0]==0:
+  output = 'YOU ARE NOT DIABETIC'
+else:
+  output = 'YOU ARE DIABETIC'
+st.title(output)
+st.subheader('Accuracy: ')
+st.write(str(accuracy_score(y_test, rf.predict(x_test))*100)+'%')
